63569dcc5ff1c09fe1d167997d1970565887f15c
latte0 <latte2573@gmail.com>
2020-04-12 18:49:53 +0900

    update leave one out

--- simple_net.py
+++ simple_net.py
@@ -19,21 +19,24 @@ class Net(nn.Module):
 
 
 
-    def __init__(self, Y):
+    def __init__(self, Y, X):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(4, 3, bias=False)
         self.Y = Y
+        self.train_X = X
 
     def leave_one_out(self, X, Xw):
         up = 0
         down = 0
-        h = 1
+        h = 0.5
         result = []
         for j, x_j in enumerate(X):
-                up += gauss( (( torch.mv(self.fc1.weight , x_j)- Xw ) / h)) * self.Y[j]
-
-        for j, x_j in enumerate(X):
-                down += gauss( (( torch.mv(self.fc1.weight , x_j)- Xw ) / h))
+                share = gauss( (( torch.mv(self.fc1.weight , x_j) - Xw ) / h ))
+                down += share
+                up += share * self.Y[j]
+                #print(share)
+                #print(self.Y[j])
+                #print(share * self.Y[j])
 
         g = up/down
         return g
@@ -41,18 +44,19 @@ class Net(nn.Module):
                 
     def forward(self, x):
         xw = self.fc1(x)
-        y = self.leave_one_out(x, xw)
+        #reluかleave_one_out切り分け
+        y = self.leave_one_out(self.train_X, xw)
         #y = F.relu(xw)
         return y
 
 iris = datasets.load_iris()
 y = np.zeros((len(iris.target), 1 + iris.target.max()), dtype=int)
 y[np.arange(len(iris.target)), iris.target] = 1
-X_train, X_test, y_train, y_test = train_test_split(iris.data, y, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(iris.data, y, test_size=0.90)
 x = Variable(torch.from_numpy(X_train).float(), requires_grad=True)
 #print(x)
 y = Variable(torch.from_numpy(y_train).float())
-net = Net(y)
+net = Net(y, x)
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 criterion = nn.MSELoss()
 
