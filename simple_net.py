8cb2768caffa96b419c6e1287e0f0d8e7691bfbb
jimbo-iplot <latte2573@gmail.com>
2020-04-15 21:21:31 +0900

    revise

--- simple_net.py
+++ simple_net.py
@@ -8,7 +8,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from torch.autograd import Variable
 
-
+#pytorchのガウス関数
 def gauss(x, a = 1, mu = 0, sigma = 1):
     return a * torch.exp(-(x - mu)**2 / (2*sigma**2))
 
@@ -19,13 +19,18 @@ class Net(nn.Module):
 
 
 
-    def __init__(self, Y, X):
+    def __init__(self, Y, X, settings):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(4, 3, bias=False)
+        #leave_ont_outのために事前に入力と出力をセットしておく
         self.Y = Y
         self.train_X = X
+        self.settings = settings
+        # バンド幅も推定する
         self.h = nn.Parameter(torch.tensor(1.0, requires_grad = True))
 
+
+    # leave_one_out推定量の計算
     def leave_one_out(self, X, Xw):
         numerator = 0
         denominator = 0
@@ -47,23 +52,33 @@ class Net(nn.Module):
     def forward(self, x):
         xw = self.fc1(x)
         #reluかleave_one_out切り分け
-        y = self.leave_one_out(self.train_X, xw)
-        #y = F.relu(xw)
+        if self.settings["activation"] == "leave_one_out":
+            y = self.leave_one_out(self.train_X, xw)
+        else:
+            y = F.relu(xw)
         return y
 
+
+#データの用意
 iris = datasets.load_iris()
 y = np.zeros((len(iris.target), 1 + iris.target.max()), dtype=int)
 y[np.arange(len(iris.target)), iris.target] = 1
 X_train, X_test, y_train, y_test = train_test_split(iris.data, y, test_size=0.95)
+
 x = Variable(torch.from_numpy(X_train).float(), requires_grad=True)
+
+# leave_one_outのために定数として一応用意しておく
 x_static = Variable(torch.from_numpy(X_train).float(), requires_grad=False)
-#print(x)
+
+
 y = Variable(torch.from_numpy(y_train).float())
-net = Net(y, x_static)
+
+#leave one outの計算のため、事前に入力と出力のパラメータをセットしておく
+net = Net(y, x_static, { "activation": "leave_one_out" } )
 optimizer = optim.SGD(net.parameters(), lr=0.01)
 criterion = nn.MSELoss()
 
-for i in range(3000):
+for i in range(100000):
     optimizer.zero_grad()
     output = net(x)
     loss = criterion(output, y)
@@ -71,7 +86,7 @@ for i in range(3000):
     loss.backward()
     optimizer.step()
 
-    # test
+    # テストデータの出力のaccuracyを学習ステップごとに行ってみる
     outputs = net(Variable(torch.from_numpy(X_test).float()))
     _, predicted = torch.max(outputs.data, 1)
     y_predicted = predicted.numpy()
