64f738652b32a4e15845cdd1d7b3ca149bd5475d
jimbo-iplot <latte2573@gmail.com>
2020-06-30 00:18:30 +0900

    add multilayer

--- multilayer_net.py
+++ multilayer_net.py
@@ -18,7 +18,6 @@ import numpy as np
 #DATA_INPUT_LENGTH = 13
 
 
-
 DATA_OUTPUT_LENGTH = 3
 DATA_MID_LENGTH = 3
 DATA_INPUT_LENGTH = 4
@@ -34,11 +33,13 @@ acc_list = []
 def gauss(x, a=1, mu=0, sigma=1):
     return a * torch.exp(-(x - mu)**2 / (2*sigma**2))
 
+
 def create_list_data(p):
     l = []
     for i in range(0, DATA_OUTPUT_LENGTH):
         l.append(p)
     return l
+
 
 def create_list_data_index(p, index):
     l = []
@@ -56,28 +57,27 @@ def predict(X):
     return np.argmax(outputs.data.numpy())
 
 
-
 def calc_inverse(x, f):
     k = -20.0
     nearest = 0.0
     while 1:
 
         res = f(k)
-        if( abs(x - res) < abs(nearest - res) ):
+        if(abs(x - res) < abs(nearest - res)):
             nearest = k
-        if( res <= x + 0.5 and  res >= x - 0.5 ):
+        if(res <= x + 0.5 and res >= x - 0.5):
             return k
-        elif ( res is None ):
+        elif (res is None):
             k += 1
         else:
             k += 1
         if(k > 20):
             return nearest
 
+
 def leave_one_out_output_inverse(x, f):
     pass
 
-    
 
 class Net(nn.Module):
 
@@ -96,6 +96,7 @@ class Net(nn.Module):
         self.settings = settings
         self.test = False
         self.middle = False
+        self.calc = False
         # バンド幅も推定する
         self.h = nn.Parameter(torch.tensor(1.5, requires_grad=True))
         self.h_middle = torch.tensor(1.0)
@@ -122,8 +123,6 @@ class Net(nn.Module):
 
         g = numerator/denominator
         return g
-
-
 
     def leave_one_out_output(self, Zw):
         numerator = 0
@@ -153,17 +152,15 @@ class Net(nn.Module):
         g = numerator/denominator
         return g
 
-
-
     def get_loo(self, index):
 
-
         def _get_loo(x):
-            input_data = torch.from_numpy(np.array(create_list_data(x))).float()
+            input_data = torch.from_numpy(
+                np.array(create_list_data(x))).float()
             output = self.leave_one_out_output(input_data)
             result = output[index]
-            
-            return result 
+
+            return result
 
         return _get_loo
 
@@ -176,24 +173,21 @@ class Net(nn.Module):
             for i in range(0, DATA_OUTPUT_LENGTH):
                 loo = self.get_loo(i)
                 M.append(calc_inverse(d[i], loo))
-            
 
             M_torch = np.array(M)
-            fc2_matrix_num_temp = self.fc2.weight.to('cpu').detach().numpy().copy()
-            fc2_matrix = np.linalg.inv( fc2_matrix_num_temp )
-            T = np.dot(fc2_matrix , M_torch)
+            fc2_matrix_num_temp = self.fc2.weight.to(
+                'cpu').detach().numpy().copy()
+            fc2_matrix = np.linalg.inv(fc2_matrix_num_temp)
+            T = np.dot(fc2_matrix, M_torch)
 
             result.append(T.tolist())
 
-        return torch.from_numpy(np.array(result)).clone().float() 
+        return torch.from_numpy(np.array(result)).clone().float()
 
     def create_middle_layer_y(self):
 
         self.M = self._create_middle_layer_y(self.Y)
         self.calc_M = self._create_middle_layer_y(self.calc_Y)
-
-        
-
 
     def set_test(self, test):
         self.test = test
@@ -201,13 +195,20 @@ class Net(nn.Module):
     def set_middle(self, middle):
         self.middle = middle
 
+    def set_calc(self, calc):
+        self.calc = calc
 
     def forward(self, x):
         if (self.middle):
             xw = self.fc1(x)
             print(self.fc1.weight)
             xw1 = self.middle_layer_leave_one_out_output(xw)
-            return xw1
+            if(not self.calc):
+                return xw1
+            else:
+                xw = self.fc2(xw)
+                y = self.leave_one_out_output(xw)
+                return y
             '''
             #print(xw1)
             weight = torch.from_numpy(self.fc2.weight.to('cpu').detach().numpy().copy()).float()
@@ -270,13 +271,12 @@ optimizer = optim.SGD(net.parameters(), lr=1.05)
 criterion = nn.MSELoss()
 
 
-test_input_x = np.linspace(-20, 20, 200)
+test_input_x = np.linspace(-50, 50, 200)
 test_input_x_list = []
 for p in test_input_x:
     test_input_x_list.append(create_list_data(p))
 
 test_input_x_torch = torch.from_numpy(np.array(test_input_x_list)).float()
-
 
 
 plt.ion()
@@ -289,32 +289,27 @@ for i in range(100):
     loss.backward()
     optimizer.step()
 
-    
     loo = net.get_loo(0)
 
-    
     net.set_test(True)
-    outputs = net(Variable(torch.from_numpy(X_test).float()))
+    outputs = net(Variable(torch.from_numpy(X_train).float()))
     _, predicted = torch.max(outputs.data, 1)
     y_predicted = predicted.numpy()
-    y_true = np.argmax(y_test, axis=1)
+    y_true = np.argmax(y_train, axis=1)
     accuracy = (int)(100 * np.sum(y_predicted == y_true) / len(y_predicted))
     print('accuracy: {0}%'.format(accuracy))
-    
 
     print(i)
-    
+
     if(i % 1 == 0):
         test_input_y_torch = net.leave_one_out_output(test_input_x_torch)
         test_input_y = test_input_y_torch.to('cpu').detach().numpy().copy()
-        #print(test_input_y)
+        # print(test_input_y)
 
         plt.plot(test_input_x, test_input_y)
         plt.pause(0.00000001)
         plt.cla()
-    
-    
-    
+
     loss_list.append(loss)
 
     # テストデータの出力のaccuracyを学習ステップごとに行ってみる
@@ -326,46 +321,47 @@ net.create_middle_layer_y()
 net.set_middle(True)
 
 for g in optimizer.param_groups:
-    g['lr'] = 0.01
+    g['lr'] = 0.005
 
-for i in range(1000):
+for i in range(10000):
+    net.set_calc(False)
     net.set_middle(True)
     net.set_test(True)
+
     optimizer.zero_grad()
     output = net(x)
     loss = criterion(output, net.M)
-    #print(net.M)
+    # print(net.M)
     print(loss)
     loss.backward()
     optimizer.step()
 
-    
     '''
     net.set_test(True)
     net.set_middle(False)
-
-    outputs = net(Variable(torch.from_numpy(X_test).float()))
-    _, predicted = torch.max(outputs.data, 1)
-    y_predicted = predicted.numpy()
-    y_true = np.argmax(y_test, axis=1)
-    accuracy = (int)(100 * np.sum(y_predicted == y_true) / len(y_predicted))
-    print('accuracy: {0}%'.format(accuracy))
-    
     '''
 
+    net.set_calc(True)
+
+    outputs = net(Variable(torch.from_numpy(X_train).float()))
+    _, predicted = torch.max(outputs.data, 1)
+    y_predicted = predicted.numpy()
+    y_true = np.argmax(y_train, axis=1)
+    accuracy = (int)(100 * np.sum(y_predicted == y_true) / len(y_predicted))
+    print('accuracy: {0}%'.format(accuracy))
+
     print(i)
-    
+
     if(i % 1 == 0):
-        test_input_y_torch = net.middle_layer_leave_one_out_output(test_input_x_torch)
+        test_input_y_torch = net.middle_layer_leave_one_out_output(
+            test_input_x_torch)
         test_input_y = test_input_y_torch.to('cpu').detach().numpy().copy()
-        #print(test_input_y)
+        # print(test_input_y)
 
         plt.plot(test_input_x, test_input_y)
         plt.pause(0.0000001)
         plt.cla()
-    
-    
-    
+
     loss_list.append(loss)
 
     # テストデータの出力のaccuracyを学習ステップごとに行ってみる
